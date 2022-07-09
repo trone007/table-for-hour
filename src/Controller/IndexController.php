bce72c14c5b1bddc0f0e556cdcf11874f1c63813
stone <scouserlfc91@gmail.com>
2022-07-09 18:48:53 +0200

    кидает на login

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,7 @@ class IndexController extends AbstractController
 {
 	/**
 	 * @Route("/", name="index_page")
+	 * @IsGranted("ROLE_USER")
 	 */
 	public function index(): Response
 	{
